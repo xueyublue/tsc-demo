9a45f94134ebd1fb6927f1065fb6e21b10489791
Xue Yu <xueyublue@gmail.com>
2023-03-12 15:58:54 +0800

    Event

--- react-tsc-demo/src/components/event/Input.tsx
+++ react-tsc-demo/src/components/event/Input.tsx
@@ -4,5 +4,8 @@ type InputProps = {
 };
 
 export default function Input(props: InputProps) {
+  //   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //     console.log(event);
+  //   };
   return <input type="text" value={props.value} onChange={props.handleChange} />;
 }
